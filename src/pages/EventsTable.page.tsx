d66576a07db45216edc0123163282cca699c929f
jpuntd <jpuntd@gmail.com>
2023-06-22 21:05:25 +0200

    wip

--- src/pages/EventsTable.page.tsx
+++ src/pages/EventsTable.page.tsx
@@ -1,31 +1,37 @@
-import { useState } from 'react'
+import { useState, FC } from 'react'
 import { Modal, Button } from "antd"
 import SchemaTable from "../components/SchemaTable";
 import SchemaForm from '../components/SchemaForm';
-import { schemaEventForm } from '../mocks/schema'
+import { eventFormSchema } from '../schema/eventForm.schema'
 
-const EventsTable = () => {
-    const [isFormModalOpen, setIsformModalOpen] = useState(false);
+const EventsTable: FC = () => {
+  const [isFormModalOpen, setIsFormModalOpen] = useState(false);
 
-    const showFormModal = () => {
-        setIsformModalOpen(true)
-    }
-    const handleFormModalCancel = () => {
-        setIsformModalOpen(false)
-    };
+  const showFormModal = () => {
+    setIsFormModalOpen(true)
+  }
+  const handleFormModalCancel = () => {
+    setIsFormModalOpen(false)
+  };
 
-    const handleOk = handleFormModalCancel
+  const mutation = useMutation({
+    mutationFn: (newEvent) => {
+      return axios.post('/todos', newTodo)
+    },
+  })
 
-    return <section>
-        <SchemaForm schema={schemaEventForm} />
-        <Button type='primary' onClick={showFormModal}>Create</Button>
-        <Modal title="Basic Modal" open={isFormModalOpen} onOk={handleOk} onCancel={handleFormModalCancel}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-        </Modal>
-        <SchemaTable />
-    </section>
+  const onSubmit = (values: Record<string, any>) => {
+    console.log(values);
+    setIsFormModalOpen(false)
+  }
+
+  return <section>
+    < Button type='primary' onClick={showFormModal} > Create</Button >
+    <Modal title="Create event" open={isFormModalOpen} onCancel={handleFormModalCancel}>
+      <SchemaForm schema={eventFormSchema} onSubmit={onSubmit} />
+    </Modal>
+    <SchemaTable />
+  </section >
 }
 
 export default EventsTable;
